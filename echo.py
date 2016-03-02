cdeaa13161594b996f42504ae389d8d1145912c1
gleken <gleken@gmail.com>
2016-03-02 18:25:07 +0300

    0.1
    fork echo

---
+++ echo.py
@@ -0,0 +1,25 @@
+import socket
+import os
+import time
+import sys
+
+
+s=socket.socket()
+
+sock.bind(('0.0.0.0', 2222))
+sock.listen(10)
+conn, addr = sock.accept()
+
+pid=os.fork()
+if pid!=0:
+	time.sleep(10)
+	sys.exit(0)
+
+while True:
+	data = conn.recv(1024)
+	if not data:
+		break
+		if data == "Close" or data == "close": conn.close()
+		conn.send(data)
+
+conn.close()
