1df6ddd9399c2709036eda6c59f9846997b18a9c
gleken <gleken@gmail.com>
2016-03-03 21:54:07 +0300

    Create echo_fork.py

---
+++ echo_fork.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import socket
+
+acceptor = socket.socket()
+acceptor.bind(('0.0.0.0', 2222))
+acceptor.listen(10)
+
+for i in range(10):
+  pid = os.fork()
+  if pid == 0:
+    childpid = os.getpid()
+    print "Child %s listening on localhost:4242" % childpid
+      try:
+        while 1:
+           
+                conn, addr = acceptor.accept()
+                data=conn.recv(1024)
+                if not data:break
+                if data == "close" or data == "Close"" conn.close()
+                conn.send(data)
+                
+                conn.close()
+               
+        except KeyboardInterrupt:
+            sys.exit()
+
+
+try:
+    os.waitpid(-1, 0)
+except KeyboardInterrupt:
+    print "\nbailing"
+    sys.exit()
