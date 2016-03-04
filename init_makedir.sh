889fde4a5bc7b57d7c3b2a77a5f958b47fb78239
gleken <gleken@gmail.com>
2016-03-04 22:11:26 +0300

    first

---
+++ init_makedir.sh
@@ -0,0 +1,12 @@
+mkdir -p //home/box/web/public/img
+mkdir -p //home/box/web/public/css
+mkdir -p //home/box/web/public/js
+mkdir -p //home/box/web/uploads
+mkdir -p //home/box/web/etc
+touch //home/box/web/public/1.jpeg
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+
+
